57697d70ac54348651a1a84aa3676ab2ece5a258
najibjodiansyah <najibjodiansyah11@gmail.com>
2024-04-21 11:16:46 +0700

    endpoint item create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,15 @@ class ItemsController < ApplicationController
     @items = Item.all
     render json: @items
   end
+
+  def create
+    item = Item.create(resource_params)
+    render json: item
+  end
+
+  private
+
+  def resource_params
+    params.require(:item).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :items, only: [:index]
+  resources :items, only: [:index, :create, :destroy]
 end
